e059fcab46789b85305ed0edcc4844952eae5b78
sergiimazurokv <sergii.mazurok.v@gmail.com>
2016-03-19 16:08:16 +0200

    fix

--- 6.js
+++ 6.js
@@ -6,12 +6,12 @@ var result = [];
 
 for (let i = 0, len = addresses.length; i < len; ++i) {
 
-  var pattern = /^ *((ул|пл|пр-т|пер)?\.? *(([^,/]+),? *( +((дом)?\.? *((\d*-?[^,/ ])((\/|,? *кв\.? *)+(\d*))*)))*?)) *$/;
+  var pattern = /^ *((ул|пл|пр-т|пер)?\.? *(([^,/]+),? *( +((дом)?\.? *((\d*-?[^,/ ])(( *\/ *|,? *кв\.? *)+(\d*))*)))*?)) *$/;
 
   var arr = addresses[i].match(pattern);
 
   result.push({
-    street: arr[4],
+    street: arr[4].trim(),
     house: arr[9] || '',
     flat: arr[12] || ''
   });
