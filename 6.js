e448361c15221e9f22cf5d9aa6cf61334a30bfc8
sergiimazurokv <sergii.mazurok.v@gmail.com>
2016-03-19 15:54:25 +0200

    res

---
+++ 6.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var addresses = require('./addresses');
+
+var result = [];
+
+for (let i = 0, len = addresses.length; i < len; ++i) {
+
+  var pattern = /^ *((ул|пл|пр-т|пер)?\.? *(([^,/]+),? *( +((дом)?\.? *((\d*-?[^,/ ])((\/|,? *кв\.? *)+(\d*))*)))*?)) *$/;
+
+  var arr = addresses[i].match(pattern);
+
+  result.push({
+    street: arr[4],
+    house: arr[9] || '',
+    flat: arr[12] || ''
+  });
+
+}
+
+module.exports = result;
+
+
+
+
+
+
+
+
